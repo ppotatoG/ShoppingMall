b4a42fe9641a080411713a9996001c3f03c47783
DaeYoung Nam <wsnam0507@naver.com>
2021-07-31 16:21:29 +0900

    chore: initial commit

---
+++ src/main/java/org/shopping/configuration/MvcConfiguration.java
@@ -0,0 +1,21 @@
+package org.shopping.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+}
